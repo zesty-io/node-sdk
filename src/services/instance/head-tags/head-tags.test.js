c97e2f9a98a88dbbd5b625c39c6cb82b2f8d4b2c
Stuart Runyan <shrunyan@gmail.com>
2019-05-24 16:02:19 -0700

    added: head tag endpoints and tests

---
+++ src/services/instance/head-tags/head-tags.test.js
@@ -0,0 +1,34 @@
+require("dotenv").config();
+
+const test = require("ava");
+const authContext = require("../../../../test/helpers/auth-context");
+
+test.beforeEach(authContext);
+
+test("getWebHeaders:200", async t => {
+  const res = await t.context.instance.getWebHeaders();
+
+  t.is(res.statusCode, 200);
+  t.truthy(Array.isArray(res.data));
+  t.truthy(res.data.length > 0);
+});
+
+test("getHeadTags:200", async t => {
+  const res = await t.context.instance.getHeadTags();
+
+  t.is(res.statusCode, 200);
+  t.truthy(Array.isArray(res.data));
+  t.truthy(res.data.length > 0);
+});
+
+test.skip("getHeadTag:200", async t => {
+  const res = await t.context.instance.getHeadTag(
+    process.env.TEST_HEAD_TAG_ZUID
+  );
+
+  console.log(res);
+
+  t.is(res.statusCode, 200);
+  t.truthy(typeof res.data === "object");
+  t.is(res.data.ZUID, process.env.TEST_HEAD_TAG_ZUID);
+});
--- src/services/instance/head-tags/index.js
+++ src/services/instance/head-tags/index.js
@@ -2,12 +2,34 @@
 
 module.exports = {
   API: {
-    // siteHeadGET: "/web/headers",
-    // headTagsGETAll: "/web/headtags",
-    // headTagsGET: "/web/headtags/HEADTAG_ZUID",
-    // headTagsDELETE: "/web/headtags/HEADTAG_ZUID",
-    // headTagsPUT: "/web/headtags/HEADTAG_ZUID",
-    // headTagsPOST: "/web/headtags",
+    getWebHeaders: "/web/headers",
+    getHeadTags: "/web/headtags",
+    getHeadTag: "/web/headtags/HEADTAG_ZUID"
+    // deleteHeadTag: "/web/headtags/HEADTAG_ZUID",
+    // updateHeadTag: "/web/headtags/HEADTAG_ZUID",
+    // createHeadTag: "/web/headtags"
   },
-  mixin: superclass => class extends superclass {}
+  mixin: superclass =>
+    class extends superclass {
+      async getWebHeaders() {
+        return await this.getRequest(this.interpolate(this.API.getWebHeaders));
+      }
+
+      async getHeadTags() {
+        return await this.getRequest(this.interpolate(this.API.getHeadTags));
+      }
+
+      async getHeadTag(headTagZUID) {
+        if (!headTagZUID) {
+          throw new Error(
+            "SDK:Instance:HeadTags:getHeadTag() missing required `headTagZUID` argument"
+          );
+        }
+        return await this.getRequest(
+          this.interpolate(this.API.fetchAuditLog, {
+            HEADTAG_ZUID: headTagZUID
+          })
+        );
+      }
+    }
 };
