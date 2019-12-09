9078307e1ea2907e1a42ed3211871cad7dc763ba
Stuart Runyan <shrunyan@gmail.com>
2019-12-09 14:14:42 -0800

    added: bind action functions onto SDK class

--- src/sdk.js
+++ src/sdk.js
@@ -5,6 +5,8 @@ const Account = require("./services/account");
 const Instance = require("./services/instance");
 const Media = require("./services/media");
 
+const actions = require("./actions");
+
 module.exports = class SDK {
   constructor(instanceZUID, token, options = {}) {
     this.instanceZUID = instanceZUID;
@@ -27,6 +29,15 @@ module.exports = class SDK {
     this.account = new Account(this.instanceZUID, this.token, this.options);
     this.instance = new Instance(this.instanceZUID, this.token, this.options);
     this.media = new Media(this.instanceZUID, this.token, this.options);
+
+    // Bind the SDK to context to all action functions
+    // Link action functions onto SDK class
+    this.action = {};
+    for (const funcName in actions) {
+      if (actions[funcName].bind) {
+        this.action[funcName] = actions[funcName].bind(this);
+      }
+    }
 
     return this.auth.verifyToken(this.token).then(res => {
       if (res.statusCode !== 200) {
