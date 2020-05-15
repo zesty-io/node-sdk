0f03acad6831e423b846c2eade34eb9649890a30
Stuart Runyan <shrunyan@gmail.com>
2020-05-14 20:27:02 -0700

    demo instance tokens

--- examples/import/index.js
+++ examples/import/index.js
@@ -1,6 +1,6 @@
 /**
  * Your ZESTY_INSTANCE_TOKEN should be stored outside of your codebase
- * and loaded via an envrionment variable since this is a private secret
+ * and loaded via an envrionment variable as this is a private secret
  */
 require("dotenv").config({
   path: "../../.env", // Change this path to your .env file location
@@ -18,22 +18,26 @@ const sdk = require("@zesty-io/sdk");
   const USER_ZUID = process.env.IMPORT_USER_ZUID; // Change this to your users ZUID
   const MODEL_ZUID = process.env.IMPORT_MODEL_ZUID; // Change this to the model ZUID on your instance
 
-  const res = await zesty.instance.createItem(MODEL_ZUID, {
-    data: {
-      field_1: "Hello Test 1", // Change this to your models field
-    },
-    web: {
-      canonicalTagMode: 1,
-      metaLinkText: "Hello Test 1",
-      metaTitle: "Meta Title Text",
-      metaKeywords: "meta,keyword,list",
-      metaDescription: "This is the meta description.",
-    },
-    meta: {
-      contentModelZUID: MODEL_ZUID,
-      createdByUserZUID: USER_ZUID,
-    },
-  });
+  try {
+    const res = await zesty.instance.createItem(MODEL_ZUID, {
+      data: {
+        field_1: "Hello Test 1", // Change this to your model fields
+      },
+      web: {
+        canonicalTagMode: 1,
+        metaLinkText: "Hello Test 1",
+        metaTitle: "Meta Title Text",
+        metaKeywords: "meta,keyword,list",
+        metaDescription: "This is the meta description.",
+      },
+      meta: {
+        contentModelZUID: MODEL_ZUID,
+        createdByUserZUID: USER_ZUID,
+      },
+    });
 
-  console.log(util.inspect(res, false, null));
+    console.log(util.inspect(res, false, null));
+  } catch (err) {
+    console.trace(err);
+  }
 })();
