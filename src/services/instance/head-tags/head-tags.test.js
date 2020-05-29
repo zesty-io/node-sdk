00dafb94aa37a0b118eb1262731440f7454dd67e
Wei-En Wong <wei-en.wong@zesty.io>
2020-05-28 18:48:09 -0700

    added headers / headtag comments

--- src/services/instance/head-tags/head-tags.test.js
+++ src/services/instance/head-tags/head-tags.test.js
@@ -5,6 +5,15 @@ const authContext = require("../../../../test/helpers/auth-context");
 
 test.beforeEach(authContext);
 
+// 
+// HEADERS 
+// 
+
+// 
+// HEADERS GET
+// 
+
+// test successful web headers retrieval
 test.serial("getWebHeaders:200", async t => {
   const res = await t.context.sdk.instance.getWebHeaders();
 
@@ -13,6 +22,15 @@ test.serial("getWebHeaders:200", async t => {
   t.truthy(res.data.length > 0);
 });
 
+// 
+// HEADTAGS 
+// 
+
+// 
+// HEADTAGS GET
+// 
+
+// test successful headtags retrieval
 test.serial("getHeadTags:200", async t => {
   const res = await t.context.sdk.instance.getHeadTags();
 
@@ -21,6 +39,11 @@ test.serial("getHeadTags:200", async t => {
   t.truthy(res.data.length > 0);
 });
 
+// 
+// HEADTAG GET
+// 
+
+// test successful headtag retrieval
 test.serial("getHeadTag:200", async t => {
   const res = await t.context.sdk.instance.getHeadTag(
     process.env.TEST_HEAD_TAG_ZUID
@@ -31,11 +54,14 @@ test.serial("getHeadTag:200", async t => {
   t.is(res.data.ZUID, process.env.TEST_HEAD_TAG_ZUID);
 });
 
-// test single headtag retrieval without specifying head tag ZUID
+// test failed headtag retrieval with no head tag ZUID
 test.serial("getHeadTag with no headTagZUID", async t => {
-  try {
-    const res = await t.context.sdk.instance.getHeadTag();  
-  } catch (err) {
-    t.is(err.message, "SDK:Instance:HeadTags:getHeadTag() missing required `headTagZUID` argument");
-  }
+  const noHeadTagZUID = await t.throwsAsync(
+    t.context.sdk.instance.getHeadTag()
+  );
+
+  t.is(
+    noHeadTagZUID.message,
+    "SDK:Instance:HeadTags:getHeadTag() missing required `headTagZUID` argument"
+  );
 });
