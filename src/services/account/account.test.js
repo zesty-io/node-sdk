20a8904aa98e3e426d81979c794585c31e89c82f
Wei-En Wong <wei-en.wong@zesty.io>
2020-05-29 11:25:56 -0700

    added comments to accounts test

--- src/services/account/account.test.js
+++ src/services/account/account.test.js
@@ -5,6 +5,15 @@ const test = require("ava");
 const authContext = require("../../../test/helpers/auth-context");
 test.beforeEach(authContext);
 
+// 
+// ACCOUNTS
+// 
+
+// 
+// ACCOUNTS GET INSTANCE
+// 
+
+// test succesful instance retrieval
 test.serial("getInstance:200", async t => {
   const res = await t.context.sdk.account.getInstance();
   t.is(res.statusCode, 200);
@@ -12,6 +21,11 @@ test.serial("getInstance:200", async t => {
   t.is(res.data.ZUID, process.env.ZESTY_INSTANCE_ZUID);
 });
 
+// 
+// ACCOUNTS GET INSTANCE
+//
+
+// test successful instance users retrieval 
 test.serial("getInstanceUsers:200", async t => {
   const res = await t.context.sdk.account.getInstanceUsers();
   t.is(res.statusCode, 200);
@@ -19,7 +33,11 @@ test.serial("getInstanceUsers:200", async t => {
   t.truthy(res.data.length > 0);
 });
 
-// tests getting site ID from local instance object
+// 
+// ACCOUNTS GET SITE ID
+// 
+
+// test successful getting site ID from local instance object
 test.serial("getSiteId:200", async t => {
   const res = await t.context.sdk.account.getSiteId();
   t.is(res, parseInt(process.env.ZESTY_SITE_ID));
