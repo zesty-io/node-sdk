addede7a9f4a2cfe4c0b51387cd73b1aa68eca7f
shrunyan <shrunyan@gmail.com>
2024-11-26 14:28:12 -0800

    add: access token support in the test auth-context helper

--- test/helpers/auth-context.js
+++ test/helpers/auth-context.js
@@ -5,19 +5,27 @@ const SDK = require("../../src/sdk");
 /**
  * Attaches an authenticated SDK on the test context
  */
-module.exports = async t => {
+module.exports = async (t) => {
   try {
-    const auth = new SDK.Auth();
-    const session = await auth.login(
-      process.env.ZESTY_USER_EMAIL,
-      process.env.ZESTY_USER_PASSWORD
-    );
+    let token;
 
-    if (!session.token) {
-      throw new Error(JSON.stringify(session));
+    if (process.env.ZESTY_INSTANCE_TOKEN) {
+      token = process.env.ZESTY_INSTANCE_TOKEN;
+    } else {
+      const auth = new SDK.Auth();
+      const session = await auth.login(
+        process.env.ZESTY_USER_EMAIL,
+        process.env.ZESTY_USER_PASSWORD
+      );
+
+      if (!session.token) {
+        throw new Error(JSON.stringify(session));
+      }
+
+      token = session.token;
     }
 
-    t.context.sdk = new SDK(process.env.ZESTY_INSTANCE_ZUID, session.token);
+    t.context.sdk = new SDK(process.env.ZESTY_INSTANCE_ZUID, token);
   } catch (error) {
     throw error;
   }
